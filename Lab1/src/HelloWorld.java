039c2e20f892e332d3425104980e35b362d2ab69
cpoptile <77410366+cpoptile@users.noreply.github.com>
2021-01-13 13:46:46 -0600

    Add files via upload

---
+++ Lab1/src/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * Course: CS1021-091
+ * Winter 2019
+ * File header contains class HelloWorld
+ * Name: poptilec
+ * Created 1/13/2021
+ */
+
+/**
+ * Course CS1021-091
+ * Winter 2019
+ * HelloWorld purpose:
+ *
+ * @author poptilec
+ * @version created on 1/13/2021 at 1:36 PM
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        //dummy code
+    }
+}
---
+++ Lab1/src/TruthAndLie.java
@@ -0,0 +1,23 @@
+/*
+ * Course: CS1011-071
+ * Fall 2019
+ * File header contains class TruthsAndLie
+ * Name: Poptile
+ * Created 09/11/2019
+ */
+
+/**
+ * Course CS1011-071
+ * Fall 2019
+ * TruthsAndLie purpose: program displays two truth statements and one lie statement
+ *
+ * @author Poptile
+ * @version created on 09/11/2019 at 8:25 PM
+ */
+public class TruthAndLie {
+    public static void main(String[] args) {
+        //Print three statements (2 Truths and 1 Lie)
+        System.out.print("I'm Romanian" + "\nI have never been out of the country"
+                + "\nI live in chicago");
+    }
+}
